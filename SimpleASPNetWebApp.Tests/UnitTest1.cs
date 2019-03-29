3079e892aa5655fa9e8485b46fa8d47690b45e87
Andrew Halil <andrew.halil@outlook.com>
2019-03-29 20:52:42 +1100

    Add project files.

---
+++ SimpleASPNetWebApp.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleASPNetWebApp.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string s = "1";
+            Assert.IsTrue(s == "1");
+        }
+
+    }
+}
